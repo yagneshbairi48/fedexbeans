7a858525a1a80caf1e2ea79e8a814cd341e37521
sande <sandeepreddybommineni2605@gmail.com>
2024-01-31 17:32:07 +0530

    Java Beans from JSON

---
+++ src/main/java/com/example/beans/TrackingNumberInfo.java
@@ -0,0 +1,34 @@
+package com.example.beans;
+
+public class TrackingNumberInfo {
+
+    private String trackingNumber;
+    private String carrierCode;
+
+    private String trackingNumberUniqueId;
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
+    public String getCarrierCode() {
+        return carrierCode;
+    }
+
+    public void setCarrierCode(String carrierCode) {
+        this.carrierCode = carrierCode;
+    }
+
+    public String getTrackingNumberUniqueId() {
+        return trackingNumberUniqueId;
+    }
+
+    public void setTrackingNumberUniqueId(String trackingNumberUniqueId) {
+        this.trackingNumberUniqueId = trackingNumberUniqueId;
+    }
+
+}
